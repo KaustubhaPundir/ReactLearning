fc9a5a02472e4f9cbcd593be514de58efb715827
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-09 12:18:06 +0530

    added handleUPclick to change state of text using settext

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -2,10 +2,17 @@ import React,{useState} from 'react'
 
 
 export default function Textform(props) {
+    const handleUPclick=()=>{
+        settext("Upper case triggered");
+    }
+    const handleonchange=()=>{
+
+    }
     const [text,settext]=useState("Enter the text");
+    // text="Text"//wrong
+    // settext("Text");//correct
     return (
         <div>
-            <form>
                 <div className="mb-3">
                     <h1>
                         {props.heading}
@@ -13,10 +20,9 @@ export default function Textform(props) {
                 </div>
                 <div className="m-3 ">
                     <label for="textarea1" className="form-label">TextArea</label>
-                    <textarea className="form-control" id="textarea1"></textarea>
+                    <textarea className="form-control" id="textarea1" value={text} onChange={handleonchange}></textarea>
                 </div>
-                <button type="submit" className="btn btn-primary">Convert to upper case</button>
-            </form>
+                <button className="btn btn-primary" onClick={handleUPclick}>Convert to upper case</button>
         </div>
     )
 }
