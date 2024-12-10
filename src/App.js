14464208724c8e7dd4770952cedd0d13f1dfb12c
Kaustubha Pundir <kaustubhapundir@gmail.com>
2024-12-10 10:27:28 +0530

    added darkmode functionality to about us

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import Textform from './components/Textform';
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About'
 
 function App() {
   return (
+    <>
     <div className="App">
       <Navbar title="Kaustubha" />
       <Textform heading="propheading"/>
     </div>
+    <div className="container my-5">
+      <About />
+    </div>
+    </>
   );
 }
 
